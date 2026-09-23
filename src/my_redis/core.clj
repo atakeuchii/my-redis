3dcf197ed588699cb5b4463e43faafdc5114315e
atakeuchii <takeuchi.akihide1110@gmail.com>
2026-09-23 15:43:36 +0900

    fix: serverとして実行できるようにする

--- src/my_redis/core.clj
+++ src/my_redis/core.clj
@@ -1,1 +1,12 @@
-(ns my-redis.core)
+(ns my-redis.core
+  (:require [my-redis.server :as server])
+  (:gen-class))
+
+(defn -main
+  "Main entry point for lein run commands"
+  [& args]
+  (case (first args)
+    "server" (let [port (Integer/parseInt (or (System/getenv "PORT") "6380"))]
+               (println "Starting my-redis server...")
+               (server/start! port))
+    (println "Usage: PORT=xxxx lein run server")))
